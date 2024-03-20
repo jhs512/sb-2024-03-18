90ca170634cd4f3f3a42586ad1f6858f9f8ab3b2
jhs512 <jangka512@gmail.com>
2024-03-20 09:44:40 +0900

    work

--- src/main/java/com/example/demo/domain/chat/chat/controller/ChatController.java
+++ src/main/java/com/example/demo/domain/chat/chat/controller/ChatController.java
@@ -56,7 +56,7 @@ public class ChatController {
 
         return messages
                 .stream()
-                .map(message -> new ChatMessageDto(message.getId(), message.getChatRoom().getId(), message.getWriter().getName(), message.getBody()))
+                .map(ChatMessageDto::new)
                 .toList();
     }
 
@@ -77,7 +77,7 @@ public class ChatController {
 
         ChatMessage chatMessage = chatService.writeMessage(chatRoom, member, createMessageReqBody.body());
 
-        ChatMessageDto chatMessageDto = new ChatMessageDto(chatMessage.getId(), chatMessage.getChatRoom().getId(), chatMessage.getWriter().getName(), chatMessage.getBody());
+        ChatMessageDto chatMessageDto = new ChatMessageDto(chatMessage);
 
         template.convertAndSend("topic", "chat" + roomId + "MessageCreated", chatMessageDto);
     }
--- src/main/java/com/example/demo/domain/chat/chat/dto/ChatMessageDto.java
+++ src/main/java/com/example/demo/domain/chat/chat/dto/ChatMessageDto.java
@@ -1,13 +1,24 @@
 package com.example.demo.domain.chat.chat.dto;
 
+import com.example.demo.domain.chat.chat.entity.ChatMessage;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
 public class ChatMessageDto {
     private long id;
     private long chatRoomId;
     private String writerName;
     private String body;
+
+    public ChatMessageDto(ChatMessage message) {
+        this.id = message.getId();
+        this.chatRoomId = message.getChatRoom().getId();
+        this.writerName = message.getWriter().getName();
+        this.body = message.getBody();
+    }
 }
